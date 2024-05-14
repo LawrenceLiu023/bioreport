e9bf8f781adac8e438f680ca87f472f00d6595f8
LawrenceLiu023 <86552368+LawrenceLiu023@users.noreply.github.com>
2024-05-14 15:16:07 +0800

    Delete dev directory

--- dev/update_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Update and install the latest version of bioreport for development
-# execute this script in the root directory of bioreport
-python ./setup.py build &&
-    echo "** The latest version has been built. **" &&
-    pip uninstall -y bioreport &&
-    echo "** The old version has been uninstalled. **" &&
-    python -m pip install ./ &&
-    echo "** The latest version has been installed. **"
