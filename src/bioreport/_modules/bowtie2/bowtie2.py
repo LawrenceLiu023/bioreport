f7d28fc3107c3342f64f71cf21be2b524b3642c5
liujiahuan <liujiahuan@ish.org.cn>
2024-08-02 18:18:15 +0800

    todo: bowtie2

---
+++ src/bioreport/_modules/bowtie2/bowtie2.py
@@ -0,0 +1,75 @@
+import re
+from typing import Hashable, Self
+
+import pandas as pd
+from pandas import Series
+
+from bioreport import _config
+from bioreport._base_module import BaseModule
+from bioreport.report import Report
+from bioreport.report_sum import ReportSum
+
+_MODULE_NAME = "bowtie2"
+
+
+class BioReportModule(BaseModule):
+    """A class for parsing report."""
+
+    def __init__(self: Self) -> None:
+        super(BioReportModule, self).__init__(
+            name=_MODULE_NAME, submodules=_config.MODULE_DICT[_MODULE_NAME], configs={}
+        )
+        self.SUBMODULE_ALIGN_INFO_LINE_PATTERN: re.Pattern = re.compile(
+            r"(?P<key>^[^:^\n]+):\s*(?P<value>[\d\%\.]+)\s*(?P<bracket>\(.+\))?\s*$"
+        )
+        self.SUBMODULE_DEDUPLICATE_INFO_LINE_PATTERN: re.Pattern = re.compile(
+            r"(?P<key>^[^:^\n]+):\s*(?P<value>[\d\%\.]+)\s*(?P<bracket>\(.+\))?\s*$"
+        )
+
+    def parse(self: Self, report: Report, name: Hashable | None = None) -> ReportSum:
+        """
+        Parse a bowtie2 report.
+
+        Parameters
+        ----------
+        report : Report
+            A bowtie2 report.
+        name : Hashable | None, default None
+            The name of `report_sum`. Default is `None`, which means `report_sum` will be named as the report file name.
+
+        Returns
+        -------
+        report_sum : ReportSum
+            A summary of the bowtie2 report.
+        """
+        module_tuple_len = 1
+        if len(report.module) != module_tuple_len:
+            raise ValueError(
+                f"The module of the report is not supported by {_MODULE_NAME} module: {str(report)}. Expected module name: {_MODULE_NAME}. Expected submodule names: {self.submodules}"
+            )
+        report_submodule: str = report.module[1]
+        if report_submodule not in self.submodules:
+            raise ValueError(
+                f"The submodule of the report is not supported by {_MODULE_NAME} module: {str(report)}. Expected submodule names: {self.submodules}"
+            )
+
+        report_sum_series: Series
+        match report_submodule:
+            case "align":
+                report_sum_series = self._submodule_align_parse(report)
+            case "deduplicate":
+                report_sum_series = self._submodule_deduplicate_parse(report)
+            case _:
+                raise ValueError(
+                    f"The submodule of the report is not supported by {_MODULE_NAME} module: {str(report)}. Expected submodule names: {self.submodules}"
+                )
+        if name is None:
+            report_sum_series.name = report.path.name
+        elif isinstance(name, str):
+            report_sum_series.name = name
+        else:
+            raise ValueError(f"The name of the report is not supported: {str(name)}")
+
+        report_sum: ReportSum = ReportSum(module=report.module, data=report_sum_series)
+
+        return report_sum
